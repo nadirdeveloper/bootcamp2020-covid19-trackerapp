deba2e3b069318de39a6375d40598f00a2739015
nadirdeveloper <63139854+nadirdeveloper@users.noreply.github.com>
2020-06-08 00:41:46 +0500

    Update Graphs.js

--- src/components/util/Graphs.js
+++ src/components/util/Graphs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pie, Doughnut } from 'react-chartjs-2';
+import {  Doughnut } from 'react-chartjs-2';
 
 export default function Graphs(props) {
     const state = {
